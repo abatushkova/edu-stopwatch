961a16ba5563f247e729622564db0cdd421e310e
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-01 23:47:35 +0300

    add stopwatch functionality

--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
-"use strict";
-exports.__esModule = true;
+const resetBtn = document.querySelector('#reset');
+const toggleBtn = document.querySelector('#toggle');
+const timer = document.querySelector('#timer');
+let ss = 0;
+let interval = null;
+let flag = 'stop';
+
+const resetTimer = () => {
+  clearInterval(interval);
+  ss = 0;
+  timer.textContent = '00:00:00';
+  toggleBtn.classList.replace('button--red', 'button--green');
+  toggleBtn.textContent = 'Start';
+  flag = 'stop';
+};
+
+const updateTimer = () => {
+  ss++;
+
+  let hrs = Math.floor(ss / 3600);
+  let mins = Math.floor((ss - hrs / 3600) / 60);
+  let secs = ss % 60;
+
+  if (hrs < 10) hrs = `0${hrs}`;
+  if (mins < 10) mins = `0${mins}`;
+  if (secs < 10) secs = `0${secs}`;
+
+  timer.textContent = `${hrs}:${mins}:${secs}`;
+};
+
+const startTimer = () => {
+  clearInterval(interval);
+
+  interval = setInterval(updateTimer, 1000);
+};
+
+const stopTimer = () => {
+  clearInterval(interval);
+}
+
+const toggleTimer = () => {
+  if (flag === 'stop') {
+    startTimer();
+    toggleBtn.classList.replace('button--green', 'button--red');
+    toggleBtn.textContent = 'Stop';
+    flag = 'start';
+  } else {
+    stopTimer();
+    toggleBtn.classList.replace('button--red', 'button--green');
+    toggleBtn.textContent = 'Start';
+    flag = 'stop';
+  }
+};
+
+resetBtn.addEventListener('click', resetTimer);
+toggleBtn.addEventListener('click', toggleTimer);
